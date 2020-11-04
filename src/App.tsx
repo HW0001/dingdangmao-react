906f382c3b4a0d83979057cbffac35d32c8a5adc
HW0001 <1600860624@qq.com>
2020-11-04 22:55:31 +0800

    增加路由，并设置简单跳转

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,63 @@
-import React from 'react';  
-   
-function App() {
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App"> 
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">账本</Link>
+            </li>
+            <li>
+              <Link to="/labels">标签</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/labels">
+            <Labels />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Redirect from="/" exact to="/home" />
+          <Route path="*">
+            <NotFund />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Labels() {
+  return <h2>Labels</h2>;
+}
+
+function Statistics() {
+  return <h2>Statistics</h2>;
+}
+function NotFund() {
+  return <h2>NotFund</h2>;
+}
